b481d39513b70547661c7fb6f7e6ce935c6bc96f
Docjeff60 <docjeff60@gmail.com>
2024-05-22 18:28:40 +0300

    newly adjusted

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,3 @@
-
-
 const root = document.documentElement;
 const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
 const marqueeContent = document.querySelector("ul.marquee-content");
@@ -8,5 +6,4 @@ root.style.setProperty("--marquee-elements", marqueeContent.children.length);
 
 for(let i=0; i<marqueeElementsDisplayed; i++) {
   marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
-};
-
+}
